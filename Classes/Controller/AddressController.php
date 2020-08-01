aa3245b2497533be9c579f16bcb818be83fca64c
Joachim Ruhs <postmaster@joachim-ruhs.de>
2020-08-01 14:27:10 +0200

    sys_language_uid available in AjaxSearch

--- Classes/Controller/AddressController.php
+++ Classes/Controller/AddressController.php
@@ -281,10 +281,10 @@ class AddressController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
 
 		$this->view->assign('id', $GLOBALS['TSFE']->id);
 
-		$languageAspect = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Context\Context::class)->getAspect('language');
-		$sys_language_uid = $languageAspect->getId();
-		$this->view->assign('L', $sys_language_uid);
-
+		$context = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Context\Context::class);
+		$sys_language_uid = $context->getPropertyFromAspect('language', 'id'); 
+		$this->view->assign('Lvar', $sys_language_uid);
+		
 //		$this->view->assign('locations', $addresses);
 		$this->view->assign('categories', $categories);
 
