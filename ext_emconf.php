cfaf5cc30499e6f165979944d51fd5d983f69b9c
Joachim Ruhs <postmaster@joachim-ruhs.de>
2021-02-11 23:36:59 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => true,
   'createDirs' => 'uploads/tx_myttaddressmap/icons',
   'clearCacheOnLoad' => 0,
-  'version' => '1.3.4',
+  'version' => '1.3.5',
   'constraints' => 
   array (
     'depends' => 
